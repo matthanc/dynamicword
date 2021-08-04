ecdd3ac697af63bc3a04f48a871fa6db20e3bdc5
Matthan Churchill <matthanc@gmail.com>
2021-08-04 10:08:17 -0700

    Add files via upload

--- blog script.R
+++ blog script.R
@@ -1,4 +1,4 @@
-# step 1 - Load packages and data
+# step 1 - load packages and data
 
 library(dplyr)
 library(readxl)
@@ -11,7 +11,7 @@ union_data <- read_excel("positiondata.xlsx", sheet = "unions")
 appointment_data <- read_excel("positiondata.xlsx", sheet = "appointment_type")
 department_data <- read_excel("positiondata.xlsx", sheet = "departments")
 
-# Step 2 - create a function for inputs
+# step 2 - create a function for inputs
 
 offerletter_inputs <- function(your_name, your_email, candidate_first_name, candidate_last_name, department_id, job_code, salary_step, appointment_type, supervisor, work_address) {
   
@@ -48,23 +48,23 @@ offerletter_inputs <- function(your_name, your_email, candidate_first_name, cand
   # join union information
   df <- left_join(df, union_data, by = "Union Code")
   
-  #join appointment information
+  # join appointment information
   df <- left_join(df, appointment_data, by = c("appointment_type" = "abbreviation"))
   
-  #join department information
+  # join department information
   df <- left_join(df, department_data, by = c("department_id" = "Department ID"))
   
   return(df)
 }
 
-# Step 3 - Create function for entering information into Microsoft Word
+# step 3 - create function for entering information into Word
 
 offerletterfunciton <- function(offerinputs) {
   
   # read the offerletter and bring to environment
   df <- read_docx("offerletter.docx")
   
-  # funcitnos to find and replace merge text (16 total)
+  # functions to find and replace merge text (16 total)
   
   # replace "letterdate" with today's date
   df <- body_replace_all_text(df,
@@ -136,7 +136,7 @@ offerletterfunciton <- function(offerinputs) {
   
   }
 
-# Step 4 Provide inputs
+# step 4 - provide inputs
 
 inputs <- offerletter_inputs(
   your_name = "Matt Churchill",
@@ -148,134 +148,14 @@ inputs <- offerletter_inputs(
   salary_step = 1,
   appointment_type = "PERM",
   supervisor = "Jane Smith",
-  work_address = "1234 Market Street"
+  work_address = "1234 Mission Street"
 )
 
 
-#Step 5 Generate document by running offer letter funciton
+# step 5 - generate document by running offer letter function
 
 offerletter <- offerletterfunciton()
 
-#Step 6 Save file - since we're using R we can specify naming convention
+# step 6 - save file with desired naming convention
 
-#save conditional offer letter
 print(offerletter, target = glue::glue("Offer Letter - {inputs$candidate_first_name} {inputs$candidate_last_name} ({inputs$appointment_type} {inputs$job_code}).docx"))
-
-
-
-#shiny server
-#your_name, your_email, candidate_first_name, candidate_last_name, department_id, job_code, salary_step, appointment_type, supervisor, work_address
-
-ui <- fluidPage(
-  textInput("your_name", "Your Name"),
-  textInput("your_email","Your Email"),
-  textInput("candidate_first_name","Candidate's First Name"),
-  textInput("candidate_last_name","Candidate's Last Name"),
-  selectInput("department_id","Department ID", unique(department_data$`Department ID`)),
-  selectInput("job_code","Job Code", unique(positions_data$`Job Code`)),
-  numericInput("salary_step", "Salary Step", value = 1, min = 1, max = 5),
-  selectInput("appointment_type","Appointment Type", unique(appointment_data$abbreviation)),
-  textInput("supervisor","Supervisor Name"),
-  textInput("work_address","Work Address (Street number and name only)"),
-  downloadButton("downloadoffer", "Download Offer Letter")
-)
-
-#attempt 2
-server <- function(input, output, session) {
-  
-  output$downloadoffer <- downloadHandler(
-    
-    
-    filename = function() glue::glue("Offer Letter - {input$candidate_first_name} {input$candidate_last_name} ({input$appointment_type} {input$job_code}).docx"),
-    
-    content = function(file) {
-      
-      inputs <- offerletter_inputs(your_name = input$your_name,
-                         your_email = input$your_email,
-                         candidate_first_name = input$candidate_first_name,
-                         candidate_last_name = input$candidate_last_name,
-                         department_id = as.double(input$department_id),
-                         job_code = as.double(input$job_code),
-                         salary_step = input$salary_step,
-                         appointment_type = input$appointment_type,
-                         supervisor = input$supervisor,
-                         work_address = input$work_address
-                         )
-      
-      # read the offerletter and bring to environment
-      df <- read_docx("offerletter.docx")
-      
-      # funcitnos to find and replace merge text (15 total)
-      
-      # replace "letterdate" with today's date
-      df <- body_replace_all_text(df,
-                                  old_value = "letterdate",
-                                  new_value = paste0(month(today(), label = TRUE, abbr = FALSE ), " ", day(today()),",", " ", year(today())))
-      
-      df <- body_replace_all_text(df,
-                                  old_value = "firstname",
-                                  new_value = inputs$candidate_first_name)
-      
-      df <- body_replace_all_text(df,
-                                  old_value = "lastname",
-                                  new_value = inputs$candidate_last_name)
-      
-      df <- body_replace_all_text(df,
-                                  old_value = "departmentname",
-                                  new_value = inputs$`Department Name`)
-      
-      df <- body_replace_all_text(df,
-                                  old_value = "appointment_abbr",
-                                  new_value = inputs$appointment_type)
-      
-      df <- body_replace_all_text(df,
-                                  old_value = "appointment_type",
-                                  new_value = inputs$appointment)
-      
-      df <- body_replace_all_text(df,
-                                  old_value = "jobcode",
-                                  new_value = as.character(inputs$job_code)) #change numeric variables to character
-      
-      df <- body_replace_all_text(df,
-                                  old_value = "jobtitle",
-                                  new_value = inputs$`Job Title`)
-      
-      df <- body_replace_all_text(df,
-                                  old_value = "unionname",
-                                  new_value = inputs$Union)
-      
-      df <- body_replace_all_text(df,
-                                  old_value = "stepnumber",
-                                  new_value = as.character(inputs$salary_step))
-      
-      df <- body_replace_all_text(df,
-                                  old_value = "hourlysalary",
-                                  new_value = as.character(inputs$hourly_salary))
-      
-      df <- body_replace_all_text(df,
-                                  old_value = "supervisorname",
-                                  new_value = inputs$supervisor)
-      
-      df <- body_replace_all_text(df,
-                                  old_value = "worklocation",
-                                  new_value = as.character(inputs$work_address))
-      
-      df <- body_replace_all_text(df,
-                                  old_value = "signdate",
-                                  new_value = paste0(month(today()+5, label = TRUE, abbr = FALSE ), " ", day(today()+5),",", " ", year(today()+5)))
-      
-      df <- body_replace_all_text(df,
-                                  old_value = "yourname",
-                                  new_value = inputs$your_name)
-      
-      df <- body_replace_all_text(df,
-                                  old_value = "youremail",
-                                  new_value = inputs$your_email)
-      
-      print(df, target = file)
-      
-    }
-  )
-}
-
-shinyApp(ui, server)
